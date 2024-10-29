03e9fd4576eaf6e9990041525e6ebf793b36f957
Luke Storry <luke.storry@faculty.ai>
2024-10-29 20:32:18 +0000

    feature icons

--- src/routes/api/data/+server.ts
+++ src/routes/api/data/+server.ts
@@ -39,8 +39,21 @@ export const POST: RequestHandler = async ({ request }) => {
     ];
 
     const features: Feature[] = [
-      { title: "Feature 1", description: "Description 1" },
-      { title: "Feature 2", description: "Description 2" },
+      {
+        title: "Feature 1",
+        description: "Description 1",
+        icon: "M13 10V3L4 14h7v7l9-11h-7z",
+      },
+      {
+        title: "Feature 2",
+        description: "Description 2",
+        icon: "M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z",
+      },
+      {
+        title: "Feature 3",
+        description: "Description 3",
+        icon: "M13 7h8m0 0v8m0-8l-8 8-4-4-6 6",
+      },
     ];
 
     // Create the content object
